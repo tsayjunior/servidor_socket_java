e7cc4b91da7d52f685e0d0b3ececc0f68a96bad3
Junior Javier <juniorllanos15@gmail.com>
2023-04-06 06:33:04 -0400

    realizando el nuevo ejercicio

---
+++ src/ejercicio_socket_marcadores_gas/Cliente.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_socket_marcadores_gas;
+
+//import ejercicio_sockets_ddr_3.*;
+//import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hp
+ */
+public class Cliente implements Runnable{
+    
+    
+    private String host;
+    private int puerto;
+    private String mensaje;
+
+    public Cliente(int puerto, String mensaje) {
+        this.host = "127.0.0.1";
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+    }
+
+    public Cliente(String host, int puerto, String mensaje) {
+        this.host = host;
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+    }
+    
+    
+
+    @Override
+    public void run() {
+//        final String HOST = "127.0.0.1"1;
+//            final int PUERTO = 5000;
+//            DataInputStream in;
+            DataOutputStream out;
+            
+        try {
+            Socket sc = new Socket(host, puerto);
+////            in = new DataInputStream(sc.getInputStream());
+            out = new DataOutputStream(sc.getOutputStream());
+            
+            //envio mensaje del cliente
+            out.writeUTF(mensaje);
+            //Recibo el mensaje del servidor
+//            String mensaje = in.readUTF();
+//            System.out.println(mensaje);
+//            
+            sc.close();
+        } catch (IOException ex) {
+            Logger.getLogger(serviidor_tcp.Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/ejercicio_socket_marcadores_gas/Servidor.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_socket_marcadores_gas;
+
+//import ejercicio_sockets_ddr_3.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import serviidor_tcp.Serviidor;
+
+/**
+ *
+ * @author hp
+ */
+public class Servidor extends Observable implements Runnable{
+
+    private ArrayList<Socket> Clientes;
+    private int puerto;
+
+    public Servidor(int puerto) {
+        this.puerto = puerto;
+        this.Clientes = new ArrayList();
+
+    }
+    
+    
+    @Override
+    public void run() {
+        ServerSocket servidor = null;
+        Socket sc= null;
+//        final int PUERTO = 5000;
+        DataInputStream in;
+        DataOutputStream out;
+
+        try {
+            servidor = new ServerSocket(puerto);
+            System.out.println("Servidor Iniciado");
+            while(true){
+                sc = servidor.accept();//este metodo se queda a la espera, esperando en esa linea, no ejecutara nada mas, hasta que venga algo
+//                //esto que devuelve es el socket del cliente, el socket del cliente es este
+                System.out.println("cliente Conectado");
+//                in = new DataInputStream(sc.getInputStream());
+////                out = new DataOutputStream(sc.getOutputStream());
+//                
+//                //leo el mensaje que me envia
+//                String mensaje = in.readUTF(); //esto se queda a la espera a que el cliente envie algo
+//                
+//                System.out.println(mensaje);
+//////                out.writeUTF("hola mundo desde el servidor !!");
+//                // metodos de los observables
+//                this.setChanged();//avisa que cambiara algo
+//                this.notifyObservers(mensaje);//lo que notifico, y se envia arriba
+//                this.clearChanged();//limpia 
+//
+//                sc.close(); //sierro el servidor del cliente
+//                System.out.println("clienteiente desconectado");
+                Clientes.add(sc);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Serviidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
