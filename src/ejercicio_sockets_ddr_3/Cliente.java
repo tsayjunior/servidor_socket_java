9d813da42b84b584cfa53d0fde0ea652c9e90c7b
Junior Javier <juniorllanos15@gmail.com>
2023-04-06 06:14:45 -0400

    aumento de variable host, a ejercicio de socket, para que su host no sea estatico

--- src/ejercicio_sockets_ddr_3/Cliente.java
+++ src/ejercicio_sockets_ddr_3/Cliente.java
@@ -19,10 +19,18 @@ import java.util.logging.Logger;
 public class Cliente implements Runnable{
     
     
+    private String host;
     private int puerto;
     private String mensaje;
 
     public Cliente(int puerto, String mensaje) {
+        this.host = "127.0.0.1";
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+    }
+
+    public Cliente(String host, int puerto, String mensaje) {
+        this.host = host;
         this.puerto = puerto;
         this.mensaje = mensaje;
     }
@@ -31,13 +39,13 @@ public class Cliente implements Runnable{
 
     @Override
     public void run() {
-        final String HOST = "127.0.0.1";
+//        final String HOST = "127.0.0.1"1;
 //            final int PUERTO = 5000;
 //            DataInputStream in;
             DataOutputStream out;
             
         try {
-            Socket sc = new Socket(HOST, puerto);
+            Socket sc = new Socket(host, puerto);
 ////            in = new DataInputStream(sc.getInputStream());
             out = new DataOutputStream(sc.getOutputStream());
             
